27dfaf66c54c178f8ebe8a513f92a06e80139bdd
Aaron Gerig <geoisborn@gmail.com>
2017-12-29 19:06:37 +0100

    Make them binaries right.

--- recipes/pimcore.php
+++ recipes/pimcore.php
@@ -13,9 +13,9 @@
 namespace Deployer;
 
 task('deploy:pimcore:install-classes', function() {
-    run('{{bin/php}} {{release_path}}/bin/console deployment:classes-rebuild -c');
+    run('{{bin/php}} {{bin/console}} deployment:classes-rebuild -c');
 });
 
 task('deploy:pimcore:migrate', function() {
-    run('{{bin/php}} {{release_path}}/bin/console pimcore:migrations:migrate');
+    run('{{bin/php}} {{bin/console}} pimcore:migrations:migrate');
 });
