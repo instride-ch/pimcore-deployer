e20edd5983ca4b31b82efa08cec1e27e7f9129b5
Aaron Gerig <a.gerig@w-vision.ch>
2018-05-28 09:48:21 +0200

    added pimcore rebuild classes task

--- recipes/pimcore.php
+++ recipes/pimcore.php
@@ -7,37 +7,35 @@
  * For the full copyright and license information, please view the LICENSE.md
  * file that is distributed with this source code.
  *
- * @copyright  Copyright (c) 2017 w-vision AG (https://www.w-vision.ch)
+ * @copyright  Copyright (c) 2018 w-vision AG (https://www.w-vision.ch)
  */
 
 namespace Deployer;
 
-
 task('deploy:pimcore:install-classes', function () {
     run('{{bin/php}} {{bin/console}} deployment:classes-rebuild -c');
+});
+
+task('deploy:pimcore:rebuild-classes', function () {
+    run('{{bin/php}} {{bin/console}} deployment:classes-rebuild -c -d');
 });
 
 task('deploy:pimcore:migrate', function () {
     run('{{bin/php}} {{bin/console}} pimcore:migrations:migrate');
 });
 
-
 task('deploy:pimcore:merge:shared', function () {
-
     $sharedFiles = get('shared_files');
     $pimcoreSharedConfigFiles = get('pimcore_shared_configurations');
 
     $all = array_merge($sharedFiles, $pimcoreSharedConfigFiles);
     set('shared_files', $all);
-
 });
 before('deploy:shared', 'deploy:pimcore:merge:shared');
 
-
-// process the pimcore config files
-// add empty array if file is empty
+// Process the pimcore config files
+// Add empty array if file is empty
 task('deploy:pimcore:shared:config', function () {
-
     $sharedPath = "{{deploy_path}}/shared";
     $emptyContent = "<?php return [];";
 
